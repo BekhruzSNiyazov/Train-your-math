e945083dec1dfe33ccc23f21355d8d4cc1e86b5f
CS50Niyazov <50236015+CS50Niyazov@users.noreply.github.com>
2019-09-01 13:06:04 +0300

    Update application.py

--- application.py
+++ application.py
@@ -109,18 +109,50 @@ def tenanswer():
 
 @app.route("/index.html")
 def first():
-    x = randrange(100)
-    y = randrange(100)
+    x = randrange(15)
+    y = randrange(15)
     global num
-    num = x + y
+    num = x * y
 
-    return render_template("index.html", x=x, y=y) + """<form action="index.html/answer" method="GET">
+    return render_template("iindex.html", x=x, y=y) + """<form action="index.html/answer" method="GET">
             <input type="number" name="answer" placeholder="Enter answer here" class="ans">
             <button class="ans">Check!</button>
         </form>"""
 
 @app.route("/index.html/quit.html")
-def quit():
+def oquit():
+    return render_template("quit.html")
+
+@app.route("/index.html/index.html/quit.html")
+def tquit():
+    return render_template("quit.html")
+
+@app.route("/index.html/index.html/index.html/quit.html")
+def thquit():
+    return render_template("quit.html")
+
+@app.route("/index.html/index.html/index.html/index.html/quit.html")
+def fquit():
+    return render_template("quit.html")
+
+@app.route("/index.html/index.html/index.html/index.html/index.html/quit.html")
+def fiquit():
+    return render_template("quit.html")
+
+@app.route("/index.html/index.html/index.html/index.html/index.html/index.html/quit.html")
+def siquit():
+    return render_template("quit.html")
+
+@app.route("/index.html/index.html/index.html/index.html/index.html/index.html/index.html/quit.html")
+def sequit():
+    return render_template("quit.html")
+
+@app.route("/index.html/index.html/index.html/index.html/index.html/index.html/index.html/index.html/quit.html")
+def equit():
+    return render_template("quit.html")
+
+@app.route("/index.html/index.html/index.html/index.html/index.html/index.html/index.html/index.html/index.html/quit.html")
+def niquit():
     return render_template("quit.html")
 
 @app.route("/index.html/index.html")
@@ -137,12 +169,12 @@ def second():
 
 @app.route("/index.html/index.html/index.html")
 def third():
-    x = randrange(100)
-    y = randrange(100)
+    x = randrange(10)
+    y = randrange(10)
     global num
-    num = x + y
+    num = x * y
 
-    return render_template("index.html", x=x, y=y) + """<form action="index.html/answer" method="GET">
+    return render_template("iindex.html", x=x, y=y) + """<form action="index.html/answer" method="GET">
             <input type="number" name="answer" placeholder="Enter answer here" class="ans">
             <button class="ans">Check!</button>
         </form>"""
@@ -161,20 +193,20 @@ def fourth():
 
 @app.route("/index.html/index.html/index.html/index.html/index.html")
 def fifth():
-    x = randrange(100)
-    y = randrange(100)
+    x = randrange(10)
+    y = randrange(10)
     global num
-    num = x + y
+    num = x * y
 
-    return render_template("index.html", x=x, y=y) + """<form action="index.html/answer" method="GET">
+    return render_template("iindex.html", x=x, y=y) + """<form action="index.html/answer" method="GET">
             <input type="number" name="answer" placeholder="Enter answer here" class="ans">
             <button class="ans">Check!</button>
         </form>"""
 
 @app.route("/index.html/index.html/index.html/index.html/index.html/index.html")
 def sixth():
-    x = randrange(100)
-    y = randrange(100)
+    x = randrange(1000)
+    y = randrange(1000)
     global num
     num = x + y
 
@@ -185,12 +217,12 @@ def sixth():
 
 @app.route("/index.html/index.html/index.html/index.html/index.html/index.html/index.html")
 def seventh():
-    x = randrange(100)
+    x = randrange(10)
     y = randrange(100)
     global num
-    num = x + y
+    num = x * y
 
-    return render_template("index.html", x=x, y=y) + """<form action="index.html/answer" method="GET">
+    return render_template("iindex.html", x=x, y=y) + """<form action="index.html/answer" method="GET">
             <input type="number" name="answer" placeholder="Enter answer here" class="ans">
             <button class="ans">Check!</button>
         </form>"""
@@ -212,17 +244,17 @@ def ninth():
     x = randrange(100)
     y = randrange(100)
     global num
-    num = x + y
+    num = x * y
 
-    return render_template("index.html", x=x, y=y) + """<form action="index.html/answer" method="GET">
+    return render_template("iindex.html", x=x, y=y) + """<form action="index.html/answer" method="GET">
             <input type="number" name="answer" placeholder="Enter answer here" class="ans">
             <button class="ans">Check!</button>
         </form>"""
 
 @app.route("/index.html/index.html/index.html/index.html/index.html/index.html/index.html/index.html/index.html/index.html")
 def tenth():
-    x = randrange(100)
-    y = randrange(100)
+    x = randrange(500)
+    y = randrange(500)
     global num
     num = x + y
 
