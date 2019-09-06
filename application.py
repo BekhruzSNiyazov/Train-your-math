bd69307496fec064fe3a6d07b0703153c515d6e4
CS50Niyazov <50236015+CS50Niyazov@users.noreply.github.com>
2019-09-06 15:55:03 +0300

    Update application.py

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from random import randrange
+from random import randrange, choice
 
 app = Flask(__name__)
 
@@ -10,22 +10,79 @@ def home():
 @app.route("/index.html/answer", methods=["GET", "POST"])
 def fanswer():
     answer = request.args.get("answer")
-    print(answer)
-    print(num)
-    if answer != str(num):
+    if int(answer) != num:
+        print(answer)
+        print(num)
+        print("INCORRECT!")
         return render_template("incorrect.html")
-    if answer == str(num):
+    if int(answer) == num:
+        print(answer)
+        print(num)
+        print("CORRECT!")
         return render_template("correct.html")
 
+@app.route("/subtraction.html/answer", methods=["GET", "POST"])
+def sanswer():
+    answer = request.args.get("answer")
+    if int(answer) != num:
+        print(answer)
+        print(num)
+        print("INCORRECT!")
+        return render_template("sincorrect.html")
+    if int(answer) == num:
+        print(answer)
+        print(num)
+        print("CORRECT!")
+        return render_template("scorrect.html")
+
+@app.route("/multiplication.html/answer", methods=["GET", "POST"])
+def manswer():
+    answer = request.args.get("answer")
+    if int(answer) != num:
+        print(answer)
+        print(num)
+        print("INCORRECT!")
+        return render_template("mincorrect.html")
+    if int(answer) == num:
+        print(answer)
+        print(num)
+        print("CORRECT!")
+        return render_template("mcorrect.html")
+
+@app.route("/division.html/answer", methods=["GET", "POST"])
+def danswer():
+    answer = request.args.get("answer")
+    if int(answer) != num:
+        print(answer)
+        print(num)
+        print("INCORRECT!")
+        return render_template("dincorrect.html")
+    if int(answer) == num:
+        print(answer)
+        print(num)
+        print("CORRECT!")
+        return render_template("dcorrect.html")
 
 @app.route("/index.html")
 def first():
+    x = randrange(100)
+    y = randrange(150)
+    global num
+    num = x + y
+
+    return render_template("index.html", x=x, y=y) + """<form action="index.html/answer" method="GET", "POST">
+            <input type="number" name="answer" placeholder="Enter answer here" class="ans">
+            <button class="ans">Check!</button>
+        </form>"""
+
+@app.route("/multiplication.html")
+def fmirst():
     x = randrange(10)
     y = randrange(15)
     global num
     num = x * y
 
-    return render_template("index.html", x=x, y=y) + """<form action="index.html/answer" method="GET", "POST">
+    return render_template("multiplication.html", x=x, y=y) + """<form action="multiplication.html/answer" method="GET", "POST">
             <input type="number" name="answer" placeholder="Enter answer here" class="ans">
             <button class="ans">Check!</button>
         </form>"""
@@ -33,3 +90,40 @@ def first():
 @app.route("/index.html/quit.html")
 def oquit():
     return render_template("quit.html")
+
+@app.route("/multiplication.html/quit.html")
+def omquit():
+    return render_template("quit.html")
+
+@app.route("/division.html/quit.html")
+def quit():
+    return render_template("quit.html")
+
+@app.route("/subtraction.html/quit.html")
+def osquit():
+    return render_template("quit.html")
+
+@app.route("/subtraction.html")
+def fsirst():
+    x = randrange(15)
+    y = randrange(10)
+    global num
+    num = x - y
+
+    return render_template("subtraction.html", x=x, y=y) + """<form action="subtraction.html/answer" method="GET", "POST">
+            <input type="number" name="answer" placeholder="Enter answer here" class="ans">
+            <button class="ans">Check!</button>
+        </form>"""
+
+@app.route("/division.html")
+def fdirst():
+    a = randrange(15)
+    b = choice(range(1, 10))
+    d = a * b
+    global num
+    num = a
+
+    return render_template("division.html", x=d, y=b) + """<form action="division.html/answer" method="GET", "POST">
+            <input type="number" name="answer" placeholder="Enter answer here" class="ans">
+            <button class="ans">Check!</button>
+        </form>"""
